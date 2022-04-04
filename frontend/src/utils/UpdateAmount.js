64a131cb2440bd889880c7221b0ad7f901347253
Joaquin <joacosanchez20@gmail.com>
2022-04-04 19:58:39 -0300

    forearch

--- frontend/src/utils/UpdateAmount.js
+++ frontend/src/utils/UpdateAmount.js
@@ -3,11 +3,11 @@ const updateAmount = (op, amount, currency, money) => {
     const moneyCopy = JSON.parse(JSON.stringify(money)); // a copy without reference to money
 
     if(op === 'Retire'){
-        moneyCopy.forEeach(obj => {if(obj.currency === currency) obj.amount -= amount})
+        moneyCopy.map(obj => {if(obj.currency === currency) obj.amount -= Number(amount)})
         return moneyCopy
     }
     if(op === 'Add'){
-        moneyCopy.forEeach(obj => {if(obj.currency === currency) obj.amount += Number(amount)})
+        moneyCopy.map(obj => {if(obj.currency === currency) obj.amount += Number(amount)})
         return moneyCopy
     }
 }
